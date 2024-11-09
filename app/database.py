f1349f51ea987134713e13d5389b1fd2b1654050
capcrz12 <carlosperezcruz01@gmail.com>
2024-11-09 19:47:59 +0100

    excepcion al conectar bd

--- app/database.py
+++ app/database.py
@@ -1,5 +1,7 @@
 import mysql.connector
 import os
+from fastapi import HTTPException
+
 
 """
 * Función para obtener una conexión a la base de datos
@@ -8,10 +10,14 @@ import os
 
 """
 def get_connection():
-    connection = mysql.connector.connect(
-        host=os.getenv('HOST'),
-        user=os.getenv('USUARIO'),
-        passwd=os.getenv('PASSWORD_DB'),
-        database=os.getenv('DATABASE')
-    )
-    return connection
+    try:
+        connection = mysql.connector.connect(
+            host=os.getenv('HOST'),
+            user=os.getenv('USUARIO'),
+            passwd=os.getenv('PASSWORD_DB'),
+            database=os.getenv('DATABASE'),
+            port=os.getenv('PORT')
+        )
+        return connection
+    except mysql.connector.Error as err:
+        raise HTTPException(status_code=500, detail=f"Error al conectar a la base de datos: {err}")
